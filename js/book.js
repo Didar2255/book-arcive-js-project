2153c61a58d997662b6a1cc8087db78fe16e03e9
Didar2255 <didar.a2255@gmail.com>
2021-09-02 01:32:50 +0600

    add book display

--- js/book.js
+++ js/book.js
@@ -19,7 +19,30 @@ loadData()
 const getBook = (books) => {
     // console.log(books)
     const displayBook = document.getElementById('book-contaner')
-    books.forEach(book => {
+    //clear display result
+    displayBook.textContent = '';
+    //error control
+    if (books.numFound === 0) {
+        const errorMessage = document.getElementById('error')
+        errorMessage.innerHTML = `
+      <p>Enter vaild book name</p>
+      `
+    }
+
+    books?.forEach(book => {
         console.log(book)
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : ''}.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h4 class="card-title"> Book name : ${book.title}</h4>
+            <h5>Author name : ${book.author_alternative_name}</h5>
+            <h5>Publish date : ${book.publish_date}</h5>
+            </div>
+        </div>
+        `
+        displayBook.appendChild(div)
     })
 }
